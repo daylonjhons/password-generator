1255d1d665ba7fe5ef5d3e82beb0714d0debd2f8
daylon <daylonjhons@daylons-MBP.home>
2024-01-08 15:42:39 -0600

    fixed typo and implemented password criteria confirmation and validation

--- Develop/script.js
+++ Develop/script.js
@@ -10,11 +10,21 @@ function writePassword() {
 
 }
 
-function generatePasswork() {
+function generatePassword() {
   var passwordLength = parseInt(prompt("enter password length (between 8 and 128 charavters):"));
 }
 
 if (isNaN(passwordLength) || passwordLength < 8 ||passwordLength > 128) {
   alert("please enter a valid password length between 8 and 128 characters.");
   return"";
+}
+
+var includeLowercase = confirm("include lowercase characters ?");
+var includeUppercase = confirm("include uppercase characters ?");
+var includeNumeric = confirm("include numeric characters ?");
+var includeSpecial = confirm("include special characters ?");
+
+if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+  alert("please select at least one character type.");
+  return "";
 }
